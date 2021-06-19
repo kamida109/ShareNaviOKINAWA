0f9310b8c1e52877ec0005e48a4687cb240865d8
kinjo <21kinjo_hayato@axiz.co.jp>
2021-06-19 13:47:26 +0900

    searchDaoの作成

---
+++ src/main/java/jp/co/group_c/dao/SearchDao.java
@@ -0,0 +1,12 @@
+package jp.co.group_c.dao;
+
+import java.util.List;
+
+import jp.co.group_c.entity.Store;
+
+public interface SearchDao {
+
+	// 店舗検索
+	public List<Store> storeSearch(String storeName, String category, String city, Integer hyouka);
+
+}
